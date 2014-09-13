5e3927423264e113ee2a759e39f61478815081be
bobjrsenior <rcarpe31@uncc.edu>
2014-09-13 19:37:27 -0400

    Added simple up/down left/right movement

--- Source/3dBulletHell/Assets/Resources/Scripts/Ship.cs
+++ Source/3dBulletHell/Assets/Resources/Scripts/Ship.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Ship : MonoBehaviour {
 
+
+	private float movement_speed;
+
 	// Use this for initialization
 	void Start () {
-	
+		movement_speed = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButton("Vertical")){
+			transform.Translate(Vector3.forward * movement_speed * Input.GetAxis("Vertical") * Time.deltaTime);
+		}
+		if(Input.GetButton("Horizontal")){
+			transform.Translate(Vector3.right * movement_speed * Input.GetAxis("Horizontal") * Time.deltaTime);
+		}
 	}
 }
