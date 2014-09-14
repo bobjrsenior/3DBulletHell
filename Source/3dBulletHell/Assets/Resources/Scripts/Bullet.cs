fa9b9bcf439d1da465a28e5be5c69a26c02ed077
bobjrsenior <rcarpe31@uncc.edu>
2014-09-14 17:43:30 -0400

    Gave Bullets a life of 5 seconds by default and a method to change it.

--- Source/3dBulletHell/Assets/Resources/Scripts/Bullet.cs
+++ Source/3dBulletHell/Assets/Resources/Scripts/Bullet.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
-	public float movement_speed;
+	private float movement_speed;
+	private float life; 
 
 	// Use this for initialization
 	public void Start () {
 		//If speed has not been set, make it 10
 		if(movement_speed == 0){
 			movement_speed = 10f;
+		}
+		if(life == 0){
+			life = 5f;
 		}
 	}
 	
@@ -22,5 +26,9 @@ public class Bullet : MonoBehaviour {
 	//Set the Bullet's speed
 	public void set_speed(float speed){
 		movement_speed = speed;
+	}
+
+	public void set_life(float seconds){
+		life = seconds;
 	}
 }
