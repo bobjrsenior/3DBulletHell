e43f0130e677a0d96d793f089dd8b41e1f2a75e0
bobjrsenior <rcarpe31@uncc.edu>
2014-09-14 17:55:27 -0400

    Added an empty object at origin for the camera to rotate around called Camera_Pivot, made it a parent of Main Camera and turned it into a prefab. I also created a script for Camera_Pivot called Camera_Rotation.cs to rotate Camera_Pivot  when you move the mouse in the X direction. Since the Main Camera is a child to Camera_Pivot, it rotates as well with Camera_Pivot which created the desired turn. x,y,z pos and x_rot changed a bit for camera as well.

---
+++ Source/3dBulletHell/Assets/Resources/Scripts/Camera_Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Rotate : MonoBehaviour {
+
+	private float rotate_speed;
+
+	// Use this for initialization
+	void Start () {
+		rotate_speed = 10f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * rotate_speed * Input.GetAxis("Mouse X") * Time.deltaTime);
+	}
+}
