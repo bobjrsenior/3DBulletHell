88cab809fc748ba3ad52a8f8fc816b3fcd78393e
bobjrsenior <rcarpe31@uncc.edu>
2014-09-13 21:09:23 -0400

    Altered the code to keep the ship within a circle. It's improved, but the ship still sometimes escaped the circle

--- Source/3dBulletHell/Assets/Resources/Scripts/Ship.cs
+++ Source/3dBulletHell/Assets/Resources/Scripts/Ship.cs
@@ -5,52 +5,80 @@ public class Ship : MonoBehaviour {
 
 
 	private float movement_speed;
+	private float h_speed;
+	private float v_speed;
+	private float x_pos;
+	private float z_pos;
 	//The distance the ship can travel from origin
 	public int room_size;
 	private int room_size_square;
 	//Have you hit the edge of the room? -1=yes 1=no
-	public int hit_edge;
+	public bool hit_edge;
 
 	// Use this for initialization
 	void Start () {
 		movement_speed = 5;
 		room_size = 10;
 		room_size_square = room_size * room_size;
-		hit_edge = 1;
+		hit_edge = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		h_speed = 0;
+		v_speed = 0;
+		x_pos = transform.position.x;
+		z_pos = transform.position.z;
 		if((transform.position.x * transform.position.x) + (transform.position.z * transform.position.z) >= room_size_square){
-			hit_edge = -1;
+			hit_edge = true;
 		}
 		else{
-			hit_edge = 1;
+			hit_edge = false;
 		}
 		if(Input.GetButton("Vertical")){
-			transform.Translate(Vector3.forward * movement_speed * hit_edge *Input.GetAxis("Vertical") * Time.deltaTime);
-			if(transform.position.z > 10){
+			v_speed = movement_speed * Input.GetAxis("Vertical") * Time.deltaTime;
+			/*if(transform.position.z > 10){
 				transform.position = new Vector3(transform.position.x, 0, 10);
 			}
 			else if(transform.position.z < -10){
 				transform.position = new Vector3(transform.position.x, 0, -10);
-			}
+			}*/
 		}
-		if((transform.position.x * transform.position.x) + (transform.position.z * transform.position.z) >= room_size_square){
-			hit_edge = -1;
-		}
-		else{
-			hit_edge = 1;
-		}
-		if(Input.GetButton("Horizontal")){
-			transform.Translate(Vector3.right * movement_speed * hit_edge * Input.GetAxis("Horizontal") * Time.deltaTime);
 
-			if(transform.position.x > 10){
+		if(Input.GetButton("Horizontal")){
+			h_speed = movement_speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+
+			/*if(transform.position.x > 10){
 				transform.position = new Vector3(10, 0, transform.position.z);
 			}
 			else if(transform.position.x < -10){
 				transform.position = new Vector3(-10, 0, transform.position.z);
+			}*/
+		}
+
+		if(hit_edge){
+			float abs_x = Mathf.Abs(transform.position.x);
+			float abs_z = Mathf.Abs(transform.position.z);
+			if(abs_x > abs_z){
+				if((h_speed < 0 && x_pos < 0) || (h_speed > 0 && x_pos > 0)){
+					h_speed *= -1;
+				}
+			}
+			else if(abs_x < abs_z){
+				if((v_speed < 0 && z_pos < 0) || (v_speed > 0 && z_pos > 0)){
+					v_speed *= -1;
+				}
+			}
+			else{
+				if((h_speed < 0 && x_pos < 0) || (h_speed > 0 && x_pos > 0)){
+					h_speed *= -1;
+				}
+				if((v_speed < 0 && z_pos < 0) || (v_speed > 0 && z_pos > 0)){
+					v_speed *= -1;
+				}
 			}
 		}
+		transform.Translate(Vector3.forward * v_speed);
+		transform.Translate(Vector3.right * h_speed);
 	}
 }
