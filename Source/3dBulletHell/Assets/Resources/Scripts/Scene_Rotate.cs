9d58064ca9867beb67ef6864bde005b00dfebe5c
bobjrsenior <rcarpe31@uncc.edu>
2014-09-16 22:58:14 -0400

    Gave Ship Player tag. Added GameObject var in Scene_Rotate called Ship and assigned it to whatever has the player tag at start (GameObject.FindWithTag("Player")). Added var to hold how much to rotate so that it isn't calculated twice a frame and rotated the Camera_Pivot and Ship by the same amount.

--- Source/3dBulletHell/Assets/Resources/Scripts/Scene_Rotate.cs
+++ Source/3dBulletHell/Assets/Resources/Scripts/Scene_Rotate.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class Scene_Rotate : MonoBehaviour {
 
+	private GameObject Ship;
+
 	private float rotate_speed;
+	private float rotation_amount;
 
 	// Use this for initialization
 	void Start () {
+		Ship = GameObject.FindWithTag("Player");
 		rotate_speed = 30f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.up * rotate_speed * Input.GetAxis("Mouse X") * Time.deltaTime);
+		rotation_amount = rotate_speed * Input.GetAxis("Mouse X") * Time.deltaTime;
+		transform.Rotate(Vector3.up * rotation_amount);
+		Ship.transform.Rotate(Vector3.up * rotation_amount);
 	}
 }
