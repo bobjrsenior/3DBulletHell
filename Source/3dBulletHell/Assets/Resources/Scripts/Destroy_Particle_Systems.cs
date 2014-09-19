a9bc9618372f1d8359cd3c5c6f511f22af3bfe68
bobjrsenior <rcarpe31@uncc.edu>
2014-09-18 23:21:57 -0400

    Created a simple script to destroy particle systems after they finish playing and added it to Orange_Bullet_Destroy and Red_Enemy_destroy.

---
+++ Source/3dBulletHell/Assets/Resources/Scripts/Destroy_Particle_Systems.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy_Particle_Systems : MonoBehaviour {
+
+	float life;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		life = particleSystem.duration + particleSystem.startLifetime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if(timer >= life){
+			Destroy(this.gameObject);
+		}
+	}
+}
